a9790233941a8ff5b6204b54498dd967c5a76752
unknown <gustavo17102005@hotmail.com>
2025-02-16 15:46:54 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -2,8 +2,8 @@
 import "dotenv/config";
 import axios from "axios"; // permite fazer qualquer requisição
 
-const { NODE_ENV } = process.env;
-const URL = NODE_ENV === "devlopment" ? "http://localhost:3000/api" : "/api";
+// const { NODE_ENV } = process.env;
+const URL = "https://deploy-jornada-full-stack-qtwd.onrender.com/api";
 
 const responseArtist = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
